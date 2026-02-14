f61221eb9620e805baf4689a9851c73a0b9818c6
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-14 17:10:43 -0300

    Criação da entidade Categoria

---
+++ src/main/java/TCC/ControleFincanceiro/entity/Categoria.java
@@ -0,0 +1,27 @@
+package TCC.ControleFincanceiro.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String icone;
+    private String cor;
+
+    @Enumerated(EnumType.STRING)
+    private TipoCategoria tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+}
