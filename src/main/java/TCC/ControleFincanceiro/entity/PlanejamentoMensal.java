871a71ee17d53dee5ce559f9f47433cdfb4ee993
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-14 18:11:25 -0300

    Criação da entidade PlanejamenoMensal

---
+++ src/main/java/TCC/ControleFincanceiro/entity/PlanejamentoMensal.java
@@ -0,0 +1,33 @@
+package TCC.ControleFincanceiro.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class PlanejamentoMensal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate referencia;
+
+    private BigDecimal rendaMensal;
+
+    private BigDecimal percentualEconomia;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "planejamento", cascade = CascadeType.ALL)
+    private List<PlanejamentoMensal> categoria;
+}
