7e36433176711ea0901fc2bc57fd05542bea61eb
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-14 18:14:48 -0300

    Criação da entidade PlanejamentoCategoria

---
+++ src/main/java/TCC/ControleFincanceiro/entity/PlanejamentoCategoria.java
@@ -0,0 +1,28 @@
+package TCC.ControleFincanceiro.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+public class PlanejamentoCategoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal limite;
+
+    @ManyToOne
+    @JoinColumn(name = "planejamento_id", nullable = false)
+    private PlanejamentoMensal planejamento;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+}
