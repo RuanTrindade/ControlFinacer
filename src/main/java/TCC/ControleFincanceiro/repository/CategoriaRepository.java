69c4989086532e5c0dae2d8a917c3315c90531ed
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-16 17:13:26 -0300

    Criando o Repository da Categoria

---
+++ src/main/java/TCC/ControleFincanceiro/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package TCC.ControleFincanceiro.repository;
+
+import TCC.ControleFincanceiro.entity.Categoria;
+import TCC.ControleFincanceiro.entity.enumerated.TipoTransacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findByUsuarioId(Long usuarioId);
+    
+    List<Categoria> findByUsuarioIdAndTipo(Long usuarioId, TipoTransacao tipo);
+
+    boolean existsByNomeAndUsuarioId(String nome, Long usuarioId);
+}
