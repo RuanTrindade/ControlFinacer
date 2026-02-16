4334f649a85d1adc43ae27c12f432013e4675330
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-16 17:43:42 -0300

    Melhoria no  Repository do ObjetivoDeposito

--- src/main/java/TCC/ControleFincanceiro/repository/ObjetivoDepositoRepository.java
+++ src/main/java/TCC/ControleFincanceiro/repository/ObjetivoDepositoRepository.java
@@ -2,10 +2,20 @@ package TCC.ControleFincanceiro.repository;
 
 import TCC.ControleFincanceiro.entity.ObjetivoDeposito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ObjetivoDepositoRepository extends JpaRepository<ObjetivoDeposito, Long> {
 
     List<ObjetivoDeposito> findByObjetivoId(Long objetivoId);
+
+    @Query("""
+SELECT COALESCE(SUM(d.valor), 0)
+FROM ObjetivoDeposito d 
+WHERE d.objetivo.id = :objetivoId
+""")
+    BigDecimal somarDepositoPorObjetivo(Long objetivoId);
+
 }
