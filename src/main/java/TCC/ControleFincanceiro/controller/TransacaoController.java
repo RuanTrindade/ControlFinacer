9ffa127ab6a7eb11431e6fc7a66679195937413c
RuanTrindade <ruansilvapereira2005@gmail.com>
2026-02-22 00:51:08 -0300

    Implementação do Atualizar no Controller

--- src/main/java/TCC/ControleFincanceiro/controller/TransacaoController.java
+++ src/main/java/TCC/ControleFincanceiro/controller/TransacaoController.java
@@ -1,6 +1,7 @@
 package TCC.ControleFincanceiro.controller;
 
 
+import TCC.ControleFincanceiro.dto.TransacaoAtualizarDTO;
 import TCC.ControleFincanceiro.dto.TransacaoCriarDTO;
 import TCC.ControleFincanceiro.dto.TransacaoResumoDTO;
 import TCC.ControleFincanceiro.entity.Transacao;
@@ -23,6 +24,17 @@ public class TransacaoController {
     public ResponseEntity<Transacao> criarTransacao(@RequestBody TransacaoCriarDTO dto) {
         Transacao nova = transacaoService.criarTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nova);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transacao> atualizarTransacao(
+            @PathVariable Long id,
+            @RequestParam Long usuarioId,
+            @RequestBody TransacaoAtualizarDTO dto
+    ) {
+        return ResponseEntity.ok(
+                transacaoService.atualizarTransacao(id, usuarioId, dto)
+        );
     }
 
     @GetMapping("/usuario/{usuarioId}")
